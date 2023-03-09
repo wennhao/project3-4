405aed2c1af13b7a4c4f5fee50c2c8ce9d4d614f
wennhao <superwenhao@gmail.com>
2023-03-09 15:44:10 +0100

    bonprinter

--- bonprinter/bonprinter.ino
+++ bonprinter/bonprinter.ino
@@ -1,6 +1,7 @@
 #include "Adafruit_Thermal.h"
-#include "SoftwareSerial.h"
+#include "bonlogo.h"
 
+#include "SoftwareSerial.h"
 #define TX_PIN 6 // Arduino TX pin -- RX on printer
 #define RX_PIN 5 // Arduino RX pin -- TX on printer
 
@@ -17,28 +18,30 @@ void setup()
   printer_connection.begin(19200);
   printer.begin();
 
-  printer.setFont('A');
-  printer.justify('C');
-  printer.setSize('L');
-  printer.boldOn();
-  printer.doubleHeightOn();
-  printer.println(F("    \nBinG\n"));
-  printer.doubleHeightOff();
-  printer.boldOff();
-  printer.setSize('S');
-  printer.println(F("Wijnhaven 107, Rotterdam"));
-  printer.println(F("--------------------------------"));
-  printer.justify('L');
-  printer.println(F("Opgenomen bedrag: "));
-  printer.println(F("IBAN: XXXXXXXXXXXXXXX-0979"));
-  printer.println(F("Pasnummer: "));
-  printer.println(F("Datum: \nTijd: "));
-  printer.justify('C');
-  printer.println(F("--------------------------------"));
-  printer.setSize('L');
-  printer.boldOn();
-  printer.doubleHeightOn();
-  printer.println(F("     \nBedankt!\n Tot ziens! :)\n   \n   \n"));
+  // Print the 75x75 pixel logo in adalogo.h:
+  printer.printBitmap(bonlogo_width, bonlogo_height, bonlogo_data);
+  //  printer.setFont('A');
+  //  printer.justify('C');
+  //  printer.setSize('L');
+  //  printer.boldOn();
+  //  printer.doubleHeightOn();
+  //  printer.println(F("    \nBinG\n"));
+  //  printer.doubleHeightOff();
+  //  printer.boldOff();
+  //  printer.setSize('S');
+  //  printer.println(F("Wijnhaven 107, Rotterdam"));
+  //  printer.println(F("--------------------------------"));
+  //  printer.justify('L');
+  //  printer.println(F("Opgenomen bedrag: "));
+  //  printer.println(F("IBAN: XXXXXXXXXXXXXXX-0979"));
+  //  printer.println(F("Pasnummer: "));
+  //  printer.println(F("Datum: \nTijd: "));
+  //  printer.justify('C');
+  //  printer.println(F("--------------------------------"));
+  //  printer.setSize('L');
+  //  printer.boldOn();
+  //  printer.doubleHeightOn();
+  //  printer.println(F("     \nBedankt!\n Tot ziens! :)\n   \n   \n"));
 }
 
 void loop() { }
